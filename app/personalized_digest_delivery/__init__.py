ca3fdd24f6f712bb0451f5d9ebe431b7fd334dc0
LucasEstrela-FullStack <l1abreuestrela@gmail.com>
2026-09-09 11:00:15 -0400

    feat: add personalized digest email delivery

---
+++ app/personalized_digest_delivery/__init__.py
@@ -0,0 +1,7 @@
+"""Personalized digest email delivery use case."""
+
+from app.personalized_digest_delivery.service import (
+    PersonalizedDigestEmailDeliveryService,
+)
+
+__all__ = ["PersonalizedDigestEmailDeliveryService"]
